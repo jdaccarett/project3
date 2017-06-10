4ad3f9b915e8a9625382e2d833d6a7bb61b873ff
andykimmay <kimand@oregonstate.edu>
2017-06-09 23:07:19 -0700

    Add files via upload

--- fast.py
+++ fast.py
@@ -20,38 +20,22 @@ def fast(tsp_prob):
                 cityArray.append(int(line[0]))
                 xArray.append(int(line[1]))
                 yArray.append(int(line[2]))
-            
-            finalList = []
-            for i in range(len(cityArray)):
-                distanceArray = []
-                distanceArray.extend([None])
-                for x in range(i):
-                    distanceArray.extend([None])
-                
-                for j in range(i, len(cityArray)-1):
-                    #input the distance using the helper function
-                    distanceArray.extend([distance(xArray[i],xArray[j+1],yArray[i],yArray[j+1])])
-                
-                finalList.append(list(distanceArray))
-            
-            #fill in other half of the array by copying it     
-            for i in range(len(cityArray)):
-                for j in range(len(cityArray)-1):
-                    finalList[j+1][i] = finalList[i][j+1]
+
+            cityArray.append(cityArray[0])
 
             #finalList is the matrix form of the graph
             tourLength = 0
-            tourArray = []
 
-            tourLength += finalList[0][len(cityArray)-1]
-            for i in range(len(cityArray)-1):
-                tourLength += finalList[i][i+1]
-                tourArray.extend([i])
-            tourArray.extend([len(cityArray)-1])
-            
+            for i in range(len(cityArray)-2):
+                tourLength += distance(xArray[i],xArray[i+1],yArray[i],yArray[i+1])
+
+            tourLength += distance(xArray[(len(cityArray)-2)],xArray[0],yArray[(len(cityArray)-2)],yArray[0])
+
+            print tourLength
+
             outputFile.write("%s\n" % tourLength)
 
-            for item in tourArray:
+            for item in cityArray:
                 outputFile.write("%s\n" % item)
 
 
@@ -60,3 +44,4 @@ def distance(x1, x2, y1, y2):
     distance = 0
     distance = math.sqrt(math.pow((x1 - x2),2) + math.pow((y1 - y2),2))
     return int(distance)
+
